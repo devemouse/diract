c9bfb4de1ab92f0561337e867b694ce5030d3aa7
Dariusz Synowiec <devemouse@gmail.com>
2011-01-25 09:37:42 +0100

    added already implemented functionality

--- Rakefile
+++ Rakefile
@@ -15,7 +15,7 @@ Jeweler::Tasks.new do |gem|
   gem.name = "diract"
   gem.homepage = "http://github.com/devemouse/diract"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
+  gem.summary = %Q{Action directories lister}
   gem.description = %Q{TODO: longer description of your gem}
   gem.email = "devemouse@gmail.com"
   gem.authors = ["Dariusz Synowiec"]
---
+++ lib/diract.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'pp'
+require 'trollop'
+
+RED      = "1;31"
+GREEN    = "32"
+YELLOW   = "33"
+
+unless RUBY_PLATFORM.include?('linux')
+   begin
+      require 'Win32/Console/ANSI' #if RUBY_PLATFORM =~ /win32/
+   rescue LoadError
+      raise 'You must gem install win32console to use color on Windows'
+   end
+end
+
+class String
+   def color(color)
+      return "\e[" + color + "m" + self + "\e[0m"
+   end
+end
+
+class Array
+  def to_hash_keys(&block)
+    Hash[*self.collect { |v|
+      [v, block.call(v)]
+    }.flatten]
+  end
+
+  def to_hash_values(&block)
+    Hash[*self.collect { |v|
+      [block.call(v), v]
+    }.flatten]
+  end
+end
+
+def load_conf(fname = "diract.conf")
+   if File.exists?(fname) then
+      file = File.new(fname,"r")
+   else
+      print 'Donfig does not exist. Creating...'
+      file = File.new(fname,"w+")
+      file.puts "c:\\Darek\\@Action\\"
+      file.rewind
+      puts 'Done.'
+   end
+   file
+end
+
+$counter = 1
+
+def rec_listdir(directory, dir_index)
+   counter = 0
+   old_dr = Dir.pwd
+   if File.directory?(directory)
+      Dir.chdir(directory)
+      files_in_dir = Dir['*']
+      described = Hash.new
+
+      if files_in_dir.empty?
+         File.delete('.diract') if File.exists?('.diract')
+      else
+         key_width = files_in_dir.max_by {|x| x.length }.length
+
+         puts
+         puts "==== (" + dir_index.color(YELLOW) + ') ' + directory.color(RED) + " ===="
+         if File.exists?('.diract')
+            #puts '.diract exists'
+
+            described = YAML::load_file('.diract')
+            #print 'described before changes: '
+            #pp described
+
+            described = files_in_dir.sort.to_hash_keys{nil}.merge(described)
+
+            #print 'merged hash: '
+            #pp described
+
+            described.delete_if {|key,val| !files_in_dir.include?(key)}
+
+            #print 'files after removal: '
+            #pp described
+
+         else
+            items_H = files_in_dir.sort.to_hash_keys{nil}
+            File.open( '.diract', 'w' ) do |out|
+               YAML.dump( items_H, out )
+            end
+            described = items_H
+         end
+
+         described.each_with_index do |pair, i|
+            puts "%#{key_width+11}s (%#{described.length+2}s): %s" % [pair[0].color(GREEN), (i+1).to_s.color(YELLOW),  pair[1]]
+         end
+
+         File.open( '.diract', 'w' ) do |out|
+            YAML.dump( described, out )
+         end
+      end
+      true
+   else
+      false
+   end
+end
+
